a3ad6eac6e88564cf90993a476f826b51ca5ddd7
Дмитрий Шаров <overlord.shor@gmail.com>
2021-02-25 13:40:58 +0300

    Рефакторинг кода

--- Assets/Scripts/MoveAnimation.cs
+++ Assets/Scripts/MoveAnimation.cs
@@ -6,20 +6,59 @@ using UnityEngine.AI;
 public class MoveAnimation : MonoBehaviour
 {
     private Rigidbody _rigidbody;
+    private NavMeshAgent _navMeshAgent;
     private Animator _animator;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
         _animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
-        var speed = _rigidbody.velocity.magnitude;
-        _animator.SetFloat("speed", speed);
+        _animator.SetFloat("speed", GetSpeed());
+        _animator.SetBool("runBack", IsRunBack());
+    }
 
-        bool isRunBack = Input.GetAxis("Vertical") < 0;
-        _animator.SetBool("runBack", isRunBack);
+    private float GetSpeed()
+    {
+        float speed = 0f;
+
+        if (!_rigidbody.isKinematic)
+        {
+            speed = _rigidbody.velocity.magnitude;
+        }
+        else if (_navMeshAgent.enabled)
+        {
+            speed = _navMeshAgent.velocity.magnitude;
+        }
+        else
+        {
+            Debug.LogError("Ошибка установки скорости для аниматора");
+        }
+
+        return speed;
+    }
+
+    private bool IsRunBack()
+    {
+        bool isRunBack = false;
+
+        if (!_rigidbody.isKinematic)
+        {
+            isRunBack = Input.GetAxis("Vertical") < 0;
+        }
+        else if (_navMeshAgent.enabled)
+        {
+            // функция постановки isRunBack для _navMeshAgent
+        }
+        else
+        {
+            Debug.LogError("Ошибка установки направления бега для аниматора");
+        }
+
+        return isRunBack;
     }
 }
