4306fc1d97b893963ead552fb1d0312df4bd3f6d
Дмитрий Шаров <overlord.shor@gmail.com>
2021-02-25 09:40:40 +0300

    Изменил движение на Rigidbody

---
+++ Assets/Scripts/PlayerKeysController.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerKeysController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1f;
+
+    private Rigidbody _rigidbody;
+
+    private float _horizontal, _vectical;
+    private Vector3 _moveVector;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vectical = Input.GetAxis("Vertical");
+
+        _moveVector = (transform.right * _horizontal + transform.forward * _vectical) * _speed / Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(_moveVector);
+    }
+}
--- Assets/Scripts/PlayerNavMeshController.cs
+++ Assets/Scripts/PlayerNavMeshController.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerNavMeshController : MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
+    private Rigidbody _rigidbody;
     private Camera _mainCamera;
 
     private void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _rigidbody = GetComponent<Rigidbody>();
         _mainCamera = Camera.main;
     }
 
@@ -19,6 +22,7 @@ public class PlayerNavMeshController : MonoBehaviour
         {
             if (Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
             {
+                _rigidbody.isKinematic = true;
                 _navMeshAgent.enabled = true;
                 _navMeshAgent.SetDestination(hit.point);
             }
@@ -26,6 +30,7 @@ public class PlayerNavMeshController : MonoBehaviour
         else if (_navMeshAgent.velocity == Vector3.zero)
         {
             _navMeshAgent.enabled = false;
+            _rigidbody.isKinematic = false;
         }
     }
 }
