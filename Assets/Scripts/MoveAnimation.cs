d17853d47ac044b8c4f62390c10c90e7920a7fc7
Дмитрий Шаров <overlord.shor@gmail.com>
2021-02-25 13:18:54 +0300

    Добавлена анимация бега вперед и назад

--- Assets/Scripts/MoveAnimation.cs
+++ Assets/Scripts/MoveAnimation.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
 public class MoveAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody _rigidbody;
+    private Animator _animator;
+
+    private void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var speed = _rigidbody.velocity.magnitude;
+        _animator.SetFloat("speed", speed);
+
+        bool isRunBack = Input.GetAxis("Vertical") < 0;
+        _animator.SetBool("runBack", isRunBack);
     }
 }
